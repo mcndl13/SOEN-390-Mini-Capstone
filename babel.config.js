ec4d6f8b7a35afe70721082a50c6b358fd6559bd
YFrancis10 <yousseffrancis03@gmail.com>
2025-02-03 23:06:52 -0500

    Changed babel file for Windows compatibility

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,8 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
   plugins: [
+  '@babel/plugin-transform-class-properties',
+  '@babel/plugin-transform-private-methods',
     ['module:react-native-dotenv', {
       moduleName: '@env',
       path: '.env',
