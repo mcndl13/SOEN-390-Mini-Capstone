717b131bdad7afa0f645e9ced0649a1f7e3962f0
Mouhamed Kairson Coundoul <mcoundoul6@gmail.com>
2025-02-10 14:52:13 -0500

    compatibility issue fix

--- lost-in-faubourg-app/babel.config.js
+++ lost-in-faubourg-app/babel.config.js
@@ -1,4 +1,20 @@
-export default function (api) {
+// export default function (api) {
+//   api.cache(true);
+//   return {
+//     presets: ["babel-preset-expo"],
+//     plugins: [
+//       [
+//         "module:react-native-dotenv",
+//         {
+//           path: ".env",
+//           safe: true,
+//           allowUndefined: false,
+//         },
+//       ],
+//     ],
+//   };
+// }
+module.exports = function (api) {
   api.cache(true);
   return {
     presets: ["babel-preset-expo"],
@@ -11,6 +27,7 @@ export default function (api) {
           allowUndefined: false,
         },
       ],
+      "expo-router/babel", // Add expo-router plugin if using Expo Router
     ],
   };
-}
+};
